d95bfa2ed5e91dc7b3115f8d85f4f2d1f110102f
Shreyas Mishra <53662675+Shreyas-Mishra@users.noreply.github.com>
2020-08-03 02:04:14 +0530

    1st-Nav updated

---
+++ react-comps/navs/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav1 from './nav1/nav1';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav1 />
+    </div>
+  );
+}
+
+export default App;
