4e3b132997b1d0bb7c04a211ea3df9643eab87ba
Shreyas Mishra <shreyas77mishra@gmail.com>
2020-10-19 22:11:23 +0530

    updates

--- react-comps/navs/src/App.js
+++ react-comps/navs/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Nav1 from './nav1/nav1';
 import { Route, Switch } from 'react-router-dom';
-import Page1 from './Pages/Page1/page1';
-import Page2 from './Pages/Page2/page2';
+// import Page1 from './Pages/Page1/page1';
+// import Page2 from './Pages/Page2/page2';
 import Home from './Pages/Home/home';
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/page1' component={Page1} />
-        <Route exact path='/page2' component={Page2} />
+        {/* <Route exact path='/page1' component={Page1} />
+        <Route exact path='/page2' component={Page2} /> */}
       </Switch>
     </div>
   );
--- react-comps/navs/src/nav1/nav1.js
+++ react-comps/navs/src/nav1/nav1.js
@@ -4,8 +4,8 @@ import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
 // import Nav2 from '../nav2/nav2';
 import './nav1.css';
 import logo from '../images/logo-white.jpg';
-import { FiShoppingCart } from "react-icons/fi";
-
+import { FiShoppingCart, FiLogOut } from "react-icons/fi";
+import { BsHeartHalf } from 'react-icons/bs';
 
 class nav1 extends Component {
 
@@ -26,18 +26,20 @@ class nav1 extends Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/page1">Nav-2</Nav.Link>
-                            <Nav.Link href="/page2">Nav-3</Nav.Link>
+                            <Nav.Link href="#page1">Nav-2</Nav.Link>
+                            <Nav.Link href="#page2">Nav-3</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 {/* <Link exact path="/Nav1/" component={Nav2} className="dropdown-item" >Nav1</Link> */}
                                 <NavDropdown.Item >Action</NavDropdown.Item>
                                 <NavDropdown.Item >Another action</NavDropdown.Item>
                                 <NavDropdown.Item >Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item >Separated link</NavDropdown.Item>
+                                <NavDropdown.Item > <FiLogOut />Logout</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
 
+                        <Nav.Link href="#link2">Contact Us</Nav.Link>
+                        <Nav.Link href="#link2"><BsHeartHalf /></Nav.Link>
                         <Nav.Link href="#link2"><FiShoppingCart /></Nav.Link>
                     </Navbar.Collapse>
                 </Navbar>
